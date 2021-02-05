9eab8fba8fd039325e5bfbea0b503c3dd9846f39
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-02-05 11:07:37 +0100

    comments
    added comments to me js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
+// declare variables
 const gifContainer = document.querySelector('.gif-container')
 const input = document.querySelector('.gif-type')
 const form = document.querySelector('.gif-form')
 
+// when 'form' is submitted, fetch the gifs
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     gifContainer.innerHTML = ''
     fetchGifs(input.value)
 })
 
+
+// declare where the data (gifs) should be fetched from
 const fetchGifs = (inputValue) => {
     const endpoint = 'https://api.giphy.com/v1/gifs/search?q='
     const key = 'JzbS3qH49ykiVKnHZx0z5M0gjNPVMbh3'
     const limit = 20
     const url = `${endpoint}${inputValue}&api_key=${key}&limit=${limit}`
 
+
+    // fetch my data from that URL
     fetch(url)
         .then(
             (response) => {
@@ -23,18 +29,32 @@ const fetchGifs = (inputValue) => {
             }
         )
 
+        // in case of error
         .catch((err) => {
             console.log(`Error: ${err}`)
         })
 
+
     const showGifs = (data) => {
+
+        // voor elk stukje data (gif)...
         data.forEach(data => {
+
+            // create container element in which my data can be put
             const containerForGif = document.createElement('div')
+
+            // set a class to that element
             containerForGif.setAttribute('class', 'containerForGif')
+
+            // append made element to existing element
             gifContainer.appendChild(containerForGif)
 
+
+            // create elements in which my data can be put
             const image = document.createElement('img')
             image.src = `https://media.giphy.com/media/${data.id}/giphy.gif`
+
+            // append made element in to previously made element
             containerForGif.appendChild(image)
         })
     }
